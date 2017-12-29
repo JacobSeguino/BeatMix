0a4c28c816dbbb9f69b3656808e53e8e96061e3a
Jacob <jacob.seguino@gmail.com>
2017-12-29 12:39:40 -0600

    added changes to script.js and presentHander.js

--- presetHandler.js
+++ presetHandler.js
@@ -1,8 +1,38 @@
 // Use this presets array inside your presetHandler
 const presets = require('./presets');
 
+const getPreset = (index) => {
+    return presets[index] || null;
+};
+
+const createOrUpdatePresent = (index, array) => {
+    if(!pressents[index]){
+        return;
+    }
+    presets[index] = array;
+    return presets[index];
+}
+
 // Complete this function:
-const presetHandler = () => {};
+const presetHandler = (method, index, newPresentArray) => {
+    if (method === 'GET'){
+        let preset = getPreset(index);
+        if (preset){
+            return [200, preset];
+        }else{
+            return [404];
+        }
+    }else if (method === 'PUT'){
+        const newPresent = createOrUpdatePresent(index, newPresentArray);
+        if (newPresent){
+            return [200, newPresent];
+        }else{
+            return [404];
+        }
+    }else{
+        return [400];
+    }
+};
 
 // Leave this line so that your presetHandler function can be used elsewhere:
 module.exports = presetHandler;
--- public/js/script.js
+++ public/js/script.js
@@ -1,6 +1,62 @@
 // Drum Arrays
-let kicks
-let snares
-let hiHats
-let rideCymbals
+const createEmptyDrumArray = () => new Array(16).fill(false);
 
+let kicks = reateEmptyDrumArray();
+let snares = reateEmptyDrumArray();
+let hiHats = reateEmptyDrumArray();
+let rideCymbals = reateEmptyDrumArray();
+
+const getDrumArrayByName = (name) => {
+    switch (name) {
+        case 'kicks':
+            return kicks;
+        case 'snares':
+            return snares;
+        case 'hiHats':
+            return hiHats;
+        case 'rideCymbals':
+            return rideCymbals;
+        default:
+            return;
+    }
+}
+
+const toggleDrum = (arrayName, index) => {
+    const drums = getDrumArrayByName(arrayName);
+    if( !drums || index > 15 || index < 0){
+        return;
+    }else{
+        drums[index] = !drums[index];
+    }
+}
+const clear = (arrayName) => {
+    const drums = getDrumArrayByName(arrayName);
+    if(drums){
+        drums.fill(false);
+    }
+}
+const invert = (arrayName) => {
+    const drums = getDrumArrayByName(arrayName);
+    if(!drums) {
+        return;
+    }
+
+    for(let i = 0; i<drums.length; i++){
+        drums[i] = !drums[i];
+    }
+}
+// const getNeighborPads = (x, y, size) => {
+//     const neighborPads = [];
+//     if (x >= size || y >= size || x < 0 || y < 0 || size < 1){
+//         return neighborPads;
+//     }
+//     neighborPads.push([x - 1, y]);
+//     neighborPads.push([x, y - 1]);
+//     neighborPads.push([x + 1, y]);
+//     neighborPads.push([x, y + 1]);
+//     return neighborPads.filter((neighbor) => {
+//         return neighbor.every((val) => {
+//             return val >= 0 && val < size;
+//         });
+//     });
+// };
